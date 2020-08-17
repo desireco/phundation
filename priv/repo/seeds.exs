91a2c19d0e1e91f0426ab9d82fa93647bd3ea8ae
Zeljko Dakic <zeljko@dakic.com>
2020-08-17 12:10:02 -0500

    db users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,11 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Rumbl.Repo
+alias Rumbl.Accounts.User
+
+Repo.insert(%User{name: "Jose", username: "josevalim"})
+Repo.insert(%User{name: "Bruce", username: "redrapids"})
+Repo.insert(%User{name: "Chris", username: "chrismccord"})
+Repo.insert(%User{name: "Zeljko", username: "desireco"})
